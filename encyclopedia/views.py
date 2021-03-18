88516fad2aab028c971209153bf853884afbdabe
Vincenzo Bonelli <bonellidevs@gmail.com>
2021-03-18 13:36:50 -0400

    not-found.html on article not found

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -10,6 +10,11 @@ def index(request):
 
 
 def wiki(request, title):
-    return render(request, "encyclopedia/single.html", {
-        "title": title
-    })
+    if util.get_entry(title):
+        return render(request, "encyclopedia/single.html", {
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/not-found.html", {
+            "title": title
+        })
