75778d21d0c682050ace3e854bc4de82e8550f12
Vincenzo Bonelli <10-10084@usb.ve>
2021-04-01 18:57:18 -0400

    README file created and AuditionListings in dev.

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -2,5 +2,11 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class AuctionListings(models.Model):
+    img = models.CharField()
+    price = models.DecimalField(decimal_places=2)
+    description = models.CharField(max_length=300)
+
+
 class User(AbstractUser):
     pass
